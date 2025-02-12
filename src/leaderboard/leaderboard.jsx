c4c0e418d91f4d154e46ad21b7845a03593683dc
Jacob Nef <jacob@nef-family.net>
2025-02-12 12:23:29 -0700

    chore: Update leaderboard page with improved formatting and player name delete old html

--- src/leaderboard/leaderboard.jsx
+++ src/leaderboard/leaderboard.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React from "react";
 
 export function Leaderboard() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>leaderboard displayed here</div>
+    <main class="container-fluid bg-dark text-center">
+      <div class="container text-center">
+        <div class="row justify-content-center">
+          <div class="col-md-6 players">
+            Player: {" "}
+            <span class="player-name">Mystery player</span>
+          </div>
+        </div>
+        <h3>Your personal best is:</h3>
+        <div class="score-count">
+          <label for="score-count">-- ms</label>
+        </div>
+        <h3>
+          Beat 5.25 ms to prove you have the Speed-Cola perk in real life!
+        </h3>
+      </div>
+      <table class="table table-light table-striped-columns">
+        <thead class="table-light">
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Time (ms)</th>
+            <th>Place</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>도윤 이</td>
+            <td>1.02</td>
+            <td>Temecula, CA</td>
+            <td>May 20, 2021</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Annie James</td>
+            <td>2.99</td>
+            <td>Provo, UT</td>
+            <td>June 2, 2021</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Gunter Spears</td>
+            <td>7.55</td>
+            <td>Reno, NV</td>
+            <td>July 3, 2020</td>
+          </tr>
+        </tbody>
+      </table>
     </main>
   );
 }
--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React from "react";
 
 export function Play() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>play displayed here</div>
+    <main class="container-fluid bg-dark text-center">
+      <div class="center-group">
+        <h1>When the screen shows an icon, click that one.</h1>
+        <h5>Tip: Don't click too early, or you'll have to restart.</h5>
+        <div class="centered-row" id="score">
+          <div class="players">
+            Player: {" "}
+            <span class="player-name">Mystery player</span>
+          </div>
+          <div class="score-count">
+            <label for="score-count">-- ms</label>
+          </div>
+        </div>
+        <button type="button" class="btn btn-secondary btn-lg">
+          Start Game
+        </button>
+      </div>
+
+      <img class="screen" src="ingameIcon.ico" alt="random" />
+
+      <div id="buttons">
+        <button type="button" class="btn btn-secondary btn-lg me-1" disabled>
+          🚘
+        </button>
+        <button type="button" class="btn btn-secondary btn-lg me-1" disabled>
+          🌼
+        </button>
+        <button type="button" class="btn btn-secondary btn-lg me-1" disabled>
+          🍏
+        </button>
+      </div>
+      <div id="player-messages">
+        <div class="event">
+          <span class="player-event">lucyLoo44</span> got a time of{" "}
+          <b>17.3 ms!</b>
+        </div>
+        <div class="event">
+          <span class="player-event">Jonohere2</span> got a time of{" "}
+          <b>3.34 ms!</b>
+        </div>
+        <div class="event">
+          <span class="system-event">game</span> connected
+        </div>
+      </div>
     </main>
   );
 }
