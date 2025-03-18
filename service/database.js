71bde459f7e91b8e75db18093ed8a5a976f13448
Jacob Nef <jacob@nef-family.net>
2025-03-18 10:43:50 -0600

    implement database and change index.js to reflect this

---
+++ service/database.js
@@ -0,0 +1,75 @@
+const { MongoClient } = require("mongodb");
+const config = require("./dbConfig.json");
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db("simon");
+const userCollection = db.collection("user");
+const scoreCollection = db.collection("score");
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(
+      `Unable to connect to database with ${url} because ${ex.message}`
+    );
+    process.exit(1);
+  }
+})();
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+async function addScore(score) {
+  return scoreCollection.insertOne(score);
+}
+
+async function getHighScores() {
+  const highScores = await scoreCollection
+    .find({})
+    .sort({ score: 1 })
+    .limit(10)
+    .toArray();
+  return highScores;
+}
+
+async function getPersonalBest(email) {
+  const scores = await getUserScores(email);
+  let personalBest = null;
+  if (scores && scores.length > 0) {
+    personalBest = Math.min(...scores.map((s) => s.score));
+  }
+  return personalBest;
+}
+
+async function getUserScores(email) {
+  if (!email) return [];
+  const userScores = await scoreCollection.find({ name: email }).toArray();
+  return userScores;
+}
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  addUser,
+  updateUser,
+  addScore,
+  getHighScores,
+  getPersonalBest,
+};
--- service/index.js
+++ service/index.js
@@ -1,16 +1,15 @@
 require("dotenv").config();
-const config = require('./dbConfig.json');
+const config = require("./dbConfig.json");
 const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcryptjs");
 const uuid = require("uuid");
+const DB = require("./database.js");
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 const authCookieName = "token";
-let users = [];
-let scores = [];
 
 app.use(express.json());
 app.use(cookieParser());
@@ -41,6 +40,7 @@ apiRouter.post("/auth/login", async (req, res) => {
         ? req.headers["x-forwarded-for"].split(",")[0].trim()
         : req.ip;
       user.location = getLocation(ip);
+      await DB.updateUser(user);
 
       setAuthCookie(res, user.token);
       res.send({ email: user.email });
@@ -54,6 +54,7 @@ apiRouter.delete("/auth/logout", async (req, res) => {
   const user = await findUser("token", req.cookies[authCookieName]);
   if (user) {
     delete user.token;
+    await DB.updateUser(user);
   }
   res.clearCookie(authCookieName);
   res.status(204).end();
@@ -69,17 +70,14 @@ const verifyAuth = async (req, res, next) => {
   }
 };
 
-apiRouter.get("/scores", verifyAuth, (_req, res) => {
+apiRouter.get("/scores", verifyAuth, async (_req, res) => {
+  const scores = await DB.getHighScores();
   res.send(scores);
 });
 
 apiRouter.get("/personal-best/:email", verifyAuth, async (req, res) => {
   const email = req.params.email;
-  const scores = await getUserScores(email);
-  let personalBest = null;
-  if (scores && scores.length > 0) {
-    personalBest = Math.min(...scores.map((s) => s.score));
-  }
+  const personalBest = await DB.getPersonalBest(email);
   res.json({ personalBest });
 });
 
@@ -87,11 +85,12 @@ apiRouter.post("/score", verifyAuth, async (req, res) => {
   const user = await findUser("email", req.body.name);
   if (user) {
     req.body.location = user.location;
+    await DB.updateUser(user);
   } else {
     return res.status(404).send({ msg: "User not found" });
   }
 
-  scores = updateScores(req.body);
+  const scores = await updateScores(req.body);
   res.send(scores);
 });
 
@@ -104,36 +103,19 @@ app.use((_req, res) => {
 });
 
 //updates and checks for inclusion in the leaderboard
-function updateScores(newScoreBody) {
-  let found = false;
-  for (const [i, prevScore] of scores.entries()) {
-    if (newScoreBody.score < prevScore.score) {
-      scores.splice(i, 0, newScoreBody);
-      found = true;
-      break;
-    }
-  }
-
-  if (!found) {
-    scores.push(newScoreBody);
-  }
-
-  if (scores.length > 10) {
-    scores.length = 10;
-  }
-
-  return scores;
-}
-
-function getUserScores(email) {
-  if (!email) return [];
-  return scores.filter((s) => s.name === email);
+async function updateScores(newScoreBody) {
+  await DB.addScore(newScoreBody);
+  const highScores = await DB.getHighScores();
+  return highScores;
 }
 
 async function findUser(field, value) {
   if (!value) return null;
 
-  return users.find((u) => u[field] === value);
+  if (field === "token") {
+    return await DB.getUserByToken(value);
+  }
+  return await DB.getUser(value);
 }
 
 async function createUser(email, password, ip) {
@@ -146,7 +128,7 @@ async function createUser(email, password, ip) {
     token: uuid.v4(),
     location: location,
   };
-  users.push(user);
+  await DB.addUser(user);
 
   return user;
 }
@@ -161,12 +143,9 @@ function setAuthCookie(res, authToken) {
 }
 
 async function getLocation(ip) {
-  console.log("IP in getLocation: ", ip);
-
   const ipstackUrl = `http://api.ipstack.com/${ip}?access_key=${config.ipWebAPIkey}`;
   const locationResponse = await fetch(ipstackUrl);
   const locationData = await locationResponse.json();
-  console.log(locationData);
   if (!locationData.city || !locationData.region_code) {
     return "Unknown";
   }
