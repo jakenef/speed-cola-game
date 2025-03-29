786fed688b3a8b0653f496377073683771eac158
Jacob Nef <jacob@nef-family.net>
2025-03-28 20:41:47 -0600

    work on peerProxy

--- service/peerProxy.js
+++ service/peerProxy.js
@@ -1,27 +1,34 @@
-const { WebSocketServer } = require('ws');
+const { WebSocketServer } = require("ws");
 
-function peerProxy(httpServer){
-    //creates a websocket object
-    const socketServer = new WebSocketServer({ server: httpServer});
+function peerProxy(httpServer) {
+  //creates a websocket object
+  const socketServer = new WebSocketServer({ server: httpServer });
 
-    socketServer.on('connection', (socket) => {
-        socket.isAlive = true;
+  socketServer.on("connection", (socket) => {
+    socket.isAlive = true;
 
-        //forward messages to everyone except the sender
-        socket.on('message', function message(data) {
-            socketServer.clients.forEach((client) => {
-                if (client !== socket && client.readyState === WebSocket.OPEN) {
-                    client.send(data);
-                }
-            });
-        });
-
-        //respond to pong messages by marking the connection alive
-        socket.on('pong', () => {
-            socket.isAlive = true;
-        });
+    //forward messages to everyone except the sender
+    socket.on("message", function message(data) {
+      socketServer.clients.forEach((client) => {
+        if (client !== socket && client.readyState === WebSocket.OPEN) {
+          client.send(data);
+        }
+      });
     });
 
-    //periodically sned out a ping message to make sure clients are alive
-    
+    //respond to pong messages by marking the connection alive
+    socket.on("pong", () => {
+      socket.isAlive = true;
+    });
+  });
+
+  //periodically sned out a ping message to make sure clients are alive
+  setInterval(() => {
+    socketServer.clients.forEach(function each(client) {
+      if (client.isAlive === false) return client.terminate();
+
+      client.isAlive = false;
+      client.ping();
+    });
+  }, 10000);
 }
