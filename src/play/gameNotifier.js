df1835d3c0586b09d0db665a3f63e6eab4b86d6b
Jacob Nef <jacob@nef-family.net>
2025-03-29 12:56:31 -0600

    work on gameNotifier

--- src/play/gameNotifier.js
+++ src/play/gameNotifier.js
@@ -1,54 +1,64 @@
 const GameEvent = {
-    System: 'system',
-    End: 'gameEnd',
-    Start: 'gameStart',
-  };
-  
-  class EventMessage {
-    constructor(from, type, value) {
-      this.from = from;
-      this.type = type;
-      this.value = value;
-    }
+  System: "system",
+  End: "gameEnd",
+  Start: "gameStart",
+};
+
+class EventMessage {
+  constructor(from, type, value) {
+    this.from = from;
+    this.type = type;
+    this.value = value;
   }
-  
-  class GameEventNotifier {
-    events = [];
-    handlers = [];
-  
-    constructor() {
-      // Simulate chat messages that will eventually come over WebSocket
-      setInterval(() => {
-        const score = Math.floor(Math.random() * 500);
-        const date = new Date().toLocaleDateString();
-        const userName = 'bach0';
-        this.broadcastEvent(userName, GameEvent.End, { name: userName, score: score, date: date });
-      }, 10000);
-    }
-  
-    broadcastEvent(from, type, value) {
-      const event = new EventMessage(from, type, value);
-      this.receiveEvent(event);
-    }
-  
-    addHandler(handler) {
-      this.handlers.push(handler);
-    }
-  
-    removeHandler(handler) {
-      this.handlers = this.handlers.filter((h) => h !== handler);
-    }
-  
-    receiveEvent(event) {
-      this.events.push(event);
-  
-      this.handlers.forEach((handler) => {
-        handler(event);
-      });
-    }
+}
+
+class GameEventNotifier {
+  events = [];
+  handlers = [];
+
+  constructor() {
+    let port = window.location.port;
+    const protocol = window.location.protocol === "http:" ? "ws" : "wss";
+    this.socket = new WebSocket(
+      `${protocol}://${window.location.hostname}:${port}/ws`
+    );
+    this.socket.onopen = (event) => {
+      this.receiveEvent(
+        new EventMessage("SpeedCola", GameEvent.System, { msg: "connected" })
+      );
+    };
+    this.socket.onclose = (event) => {
+      this.receiveEvent(new EventMessage('SpeedCola', GameEvent.System, { msg: 'disconnected' }));
+    };
+    this.socket.onmessage = async (msg) => {
+      try {
+        const event = JSON.parse(await msg.data.text());
+        this.receiveEvent(event);
+      } catch {}
+    };
   }
 
-  
-  const GameNotifier = new GameEventNotifier();
-  export { GameEvent, GameNotifier };
-  
+  broadcastEvent(from, type, value) {
+    const event = new EventMessage(from, type, value);
+    this.receiveEvent(event);
+  }
+
+  addHandler(handler) {
+    this.handlers.push(handler);
+  }
+
+  removeHandler(handler) {
+    this.handlers = this.handlers.filter((h) => h !== handler);
+  }
+
+  receiveEvent(event) {
+    this.events.push(event);
+
+    this.handlers.forEach((handler) => {
+      handler(event);
+    });
+  }
+}
+
+const GameNotifier = new GameEventNotifier();
+export { GameEvent, GameNotifier };
